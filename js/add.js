3b7604e9c3072722f88a1e992e8315440772ca84
AsilAshraf <asilashraf4664@gmail.com>
2019-03-26 15:33:04 +0530

    greater number

---
+++ js/add.js
@@ -0,0 +1,4 @@
+var x=150;
+var y=250;
+var z=x+y;
+console.log(z);
---
+++ js/greaternum.js
@@ -0,0 +1,17 @@
+var num1=100;
+var num2=200;
+var num3=350;
+
+if(num1>num2)
+{
+    if(num1>num3)
+    {
+    console.log(num1);
+}}
+else
+    if(num2>num3){
+    console.log(num2);
+}
+else{
+    console.log(num3);
+}
