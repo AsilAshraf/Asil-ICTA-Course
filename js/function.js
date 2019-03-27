5a1a7559ce0d865b596b9517f511ab0c3810cd1d
AsilAshraf <asilashraf4664@gmail.com>
2019-03-27 10:32:59 +0530

    functions

---
+++ js/function.js
@@ -0,0 +1,18 @@
+var num1=150;
+var num2=200;
+var num3=add(num1,num2);
+console.log(num3);
+var num4=sub(num1,num3)
+console.log(num4);
+
+function add(x,y)
+{
+
+z=x+y;
+return z;
+}
+function sub(x,y)
+{
+    m=x-y;
+    return m;
+}
