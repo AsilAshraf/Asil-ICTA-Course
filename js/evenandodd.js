a614562005a38ba7f4d504182d30ad7710b87d66
AsilAshraf <asilashraf4664@gmail.com>
2019-03-27 09:59:05 +0530

    reverse

---
+++ js/evenandodd.js
@@ -0,0 +1,16 @@
+var a=12;
+var b=13;
+if(a%2==0)
+{
+    console.log("a is even number")
+}
+else
+{console.log("a is odd number")
+}
+if(b%2==0)
+{
+    console.log("b is even number")
+}
+else{
+    console.log("b is odd number")
+}
