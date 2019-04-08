1f7d9e98545bb8927c0f4149dcd1cce6f9751947
AsilAshraf <asilashraf4664@gmail.com>
2019-04-08 16:25:56 +0530

    noderoutjsonsetting

--- nodejs/app.js
+++ nodejs/app.js
@@ -8,7 +8,7 @@ app.get('/home', (req,res)=>{
     res.send("welcome to my home")
 });
 app.get('/contact', (req,res)=>{
-    res.send("contact us")
+    res.send('[{"name":"ram","gender":"male"},{"name":"raju","age":24}]')
 });
 
 app.listen(2500);
