4ceb354d182604adcb60712c813d1c517b005da3
AsilAshraf <asilashraf4664@gmail.com>
2019-04-09 11:51:14 +0530

    Layoutsetting

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,14 +1,24 @@
 const express=require('express');
+const exphbs=require('express-handlebars')  //for setting template engine ie handlebars
 
 var app=express();
+
+app.engine('handlebars',exphbs({defaultLayout:'mainpage'}));   //code for template engine setting  //default layout setting also
+app.set('view engine','handlebars');  //viewing of template engine
+
+
 app.get('/', (req,res)=>{
-    res.send("welcome")
+    res.render('index')  //rendering and calling of the rout page
 });
 app.get('/home', (req,res)=>{
     res.send("welcome to my home")
 });
 app.get('/contact', (req,res)=>{
     res.send('[{"name":"ram","gender":"male"},{"name":"raju","age":24}]')
+});
+app.get('/about',(req,res)=>{
+    res.render('about')
+
 });
 
 app.listen(2500);
