9d668ef48f9f26303a4f160c527771773aa80574
AsilAshraf <asilashraf4664@gmail.com>
2019-04-09 16:14:26 +0530

    anishflaskcompleted

--- nodejs/app.js
+++ nodejs/app.js
@@ -6,7 +6,8 @@ var app=express();
 app.engine('handlebars',exphbs({defaultLayout:'mainpage'}));   //code for template engine setting  //default layout setting also
 app.set('view engine','handlebars');  //viewing of template engine
 
-
+app.use(express.static('views/static'));  //image path assigning views folder and its sub folder static
+app.use(express.urlencoded());  // data fetching from form
 app.get('/', (req,res)=>{
     res.render('index')  //rendering and calling of the rout page
 });
@@ -14,11 +15,24 @@ app.get('/home', (req,res)=>{
     res.send("welcome to my home")
 });
 app.get('/contact', (req,res)=>{
-    res.send('[{"name":"ram","gender":"male"},{"name":"raju","age":24}]')
+    res.render('contact')
 });
 app.get('/about',(req,res)=>{
     res.render('about')
 
 });
+app.get('/gallery',(req,res)=>{
+    res.render('gallery')
+});
 
+app.post('/getdata',(req,res)=>{  //transfer contents after button clicking into getdata
+   var name=req.body.getname;  //data fetched from getname and its add into variable name
+   var address=req.body.getaddr;
+   var place=req.body.getplace;
+   var email=req.body.getemail;
+   var mobile=req.body.getmob;
+
+   console.log(name,address,place,email,mobile);
+    //res.send(name)
+});
 app.listen(2500);
